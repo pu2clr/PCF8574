54fd1914d50057e13e928f51a2cb586c00ecfecf
pu2clr <pu2clr@gmail.com>
2021-02-22 14:46:59 -0300

    PCF8574 Arduino Library

--- examples/poc_00/poc_00.ino
+++ examples/poc_00/poc_00.ino
@@ -1,4 +1,24 @@
+/**
+ *  This sketch checks if the status of the ports 4, 5, 6 or  7 is LOW (button pressed)  
+ *  Use the serial monitor to check the results.
+ *   
+ *  Circuit Arduino and PCF8574 setup
+ *  
+ *  | Device   | PCF8574  | Description                               |
+ *  | -------- | -------- | ----------------------------------------- |
+ *  | Arduino  |          |                                           |
+ *  |    A5    | SCL (14) | I2C Clock                                 |
+ *  |    A4    | SDA (15) | I2C Data                                  |
+ *  | -------- |          |                                           |
+ *  | Buttons  |          |                                           |
+ *  |   B0     |  PORT 4  | Button connected to the GND               |
+ *  |   B1     |  PORT 5  | Button connected to the GND               |
+ *  |   B2     |  PORT 6  | Button connected to the GND               |
+ *  |   B3     |  PORT 7  | Button connected to the GND               |
 
+ *  
+ *  Author: Ricardo LIma Caratti, 2021.
+ */
 #include <pu2clr_pcf8574.h>
 
 PCF pcf;
@@ -7,19 +27,19 @@ void setup()
     Serial.begin(9600); // The baudrate of Serial monitor is set in 9600
     while (!Serial);    // Waiting for Serial Monitor
     
-    pcf.setup(0x20); 
-    pcf.write(0B00000000); // Turns all pins/ports LOW
-    Serial.print("Press a button or make one of the ports 4, 5, 6 or 7 HIGH (VCC)\n"); 
+    pcf.setup(0x20);    // The PCF8574 is configured to 0x20 I2C address. Check A0, A1 and A2 pins of the device.
+    pcf.write(0B11111111); // Turns all pins/ports HIGH
+    Serial.print("Press a button or make one of the ports 4, 5, 6 or 7 LOW (GND)\n"); 
 
 }
 
 void loop()
 {
   for (uint8_t i = 4; i < 8; i++ ) {
-    if ( pcf.digitalRead(i) == HIGH) {
+    if ( pcf.digitalRead(i) == LOW) {
       Serial.print("\nThe port ");
       Serial.print(i);
-      Serial.print(" is HIGH (button pressed)");
+      Serial.print(" is LOW (button pressed)");
     }
   }
 }
---
+++ examples/poc_01_interrupt/poc_01.ino
@@ -0,0 +1,94 @@
+/**
+ * This sketch controls two LEDs (ports 0 and 1)  and 4 buttons (ports 4 ,5, 6 and 7)
+ * The LEDs are connected to the +VCC via 1K resistor and the ports 0 and 1 of the device.  
+ * 
+ * The PCF8574 INT pin is connected to the Arduino Nano pin 2 (with support to interrupt). 
+ * When any port chanes their status (from LOW to HIGH or HIGH to LOW), an interrupt is fired. 
+ * 
+ * When you press a button the LEDS will blinks in different ways.
+ * 
+ * 
+ *   Circuit Arduino and PCF8574 setup
+ *
+ *  | Device   | PCF8574  | Description                               |
+ *  | -------- | -------- | ----------------------------------------- |
+ *  | Arduino  |          |                                           |
+ *  |    A5    | SCL (14) | I2C Clock                                 |
+ *  |    A4    | SDA (15) | I2C Data                                  |
+ *  |    D2    | INT (13) | Arduino Interrupt pin                     |
+ *  | -------- |          |                                           |
+ *  | Buttons  |          |                                           |
+ *  |   B0     |  PORT 4  | Button connected to the GND               |
+ *  |   B1     |  PORT 5  | Button connected to the GND               |
+ *  |   B2     |  PORT 6  | Button connected to the GND               |
+ *  |   B3     |  PORT 7  | Button connected to the GND               |
+ *  | -------- |          |                                           |
+ *  | L E D s  |          |                                           |
+ *  |  LED 0   |  PORT 0  | LED eonnected to the +VCC via 1K resistor |
+ *  |  LED 1   |  PORT 1  | LED eonnected to the +VCC via 1K resistor |
+ * 
+ */
+
+#include <pu2clr_pcf8574.h>
+
+#define ARDUINO_INTERRUPT_PIN 2
+
+volatile int interrupt_count = 0;
+volatile bool pcf_event =  false;
+
+PCF pcf;
+
+void setup()
+{
+    Serial.begin(9600); // The baudrate of Serial monitor is set in 9600
+    while (!Serial);    // Waiting for Serial Monitor
+    pcf.setup(); // The defaul I2C address is 0x20. Check your PCF8574 device A0, A1 and A2 pins
+    pcf.write(0B11111111); // Turns all PCF8574 ports HIGH   
+
+    pinMode(ARDUINO_INTERRUPT_PIN, INPUT_PULLUP); // Set Arduino internal pull up resistor to ARDUINO_INTERRUPT_PIN
+    attachInterrupt(digitalPinToInterrupt(ARDUINO_INTERRUPT_PIN), checkInterrupt, FALLING); // Will call checkInterrupt when PCF8574 INT pin is LOW
+
+    Serial.print("\nStarting");
+}
+
+/**
+   Proccess the PCF8574 event
+   This function is called when any port of the PCF8574 is changed from HIGH to LOW and LOW to HIGH.
+   The PCF8574 INT pin goes to LOW when any port status is changed. 
+*/
+void checkInterrupt()
+{
+  pcf_event = true;
+  interrupt_count++;
+}
+
+void blinkLED(int delay_value) {
+  for ( int i = 0; i < 3; i ++ ) {
+    pcf.digitalWrite(0,LOW);
+    delay(delay_value);
+    pcf.digitalWrite(0,HIGH);
+    delay(delay_value);
+    pcf.digitalWrite(1,LOW);
+    delay(delay_value);
+    pcf.digitalWrite(1,HIGH);   
+    delay(delay_value); 
+  }
+        
+}
+
+void loop()
+{
+  if ( pcf_event ) {
+    // Checks what port is LOW
+    for ( uint8_t i = 4; i < 8; i++) {
+      if ( pcf.digitalRead(i) == LOW ) {
+         Serial.print("\nThe button pressed was: ");
+         Serial.print(i);
+         blinkLED(i * i * i * i); // calculates  different times to each button 
+      }
+    }
+    pcf.write(0B11111111); // Turns all ports HIGH
+    pcf_event = false; // Cleans the interrupt status
+  }  
+
+}
